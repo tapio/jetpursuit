428627c899497f5f2e9fb8d04cada00ea77ad671
Tapio Vierros <tapiovierros@gmail.com>
2013-02-11 21:34:43 +0200

    Clouds.

--- js/world.js
+++ js/world.js
@@ -1,6 +1,7 @@
 
 function World(scene) {
 	var w = 5000, h = 5000;
+	// Sea
 	var seaTex = THREE.ImageUtils.loadTexture("assets/water.jpg");
 	seaTex.wrapS = seaTex.wrapT = THREE.RepeatWrapping;
 	seaTex.repeat.set(2, 2);
@@ -8,6 +9,28 @@ function World(scene) {
 	var seaGeo = new THREE.PlaneGeometry(w, h);
 	var sea = new THREE.Mesh(seaGeo, seaMat);
 	scene.add(sea);
+
+	// Clouds
+	var numClouds = 1000;
+	var cloudTex = THREE.ImageUtils.loadTexture("assets/cloud1.png");
+	var cloudGeo = new THREE.Geometry();
+	var cloudMat = new THREE.ParticleBasicMaterial({
+		size: 128,
+		map: cloudTex,
+		depthTest: true,
+		transparent: true,
+		opacity: 0.9
+	});
+	for (var i = 0; i < numClouds; ++i) {
+		var vertex = new THREE.Vector3();
+		vertex.x = Math.random() * 2000 - 1000;
+		vertex.y = Math.random() * 2000 - 1000;
+		vertex.z = Math.random() * 2000 + 1000;
+		cloudGeo.vertices.push(vertex);
+	}
+	var clouds = new THREE.ParticleSystem(cloudGeo, cloudMat);
+	clouds.sortParticles = true;
+	scene.add(clouds);
 
 	this.update = function(position) {
 		sea.position.x = position.x;
