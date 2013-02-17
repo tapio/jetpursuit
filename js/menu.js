8b85a422bc86ac5a0207a2bbbd5b9db899fa8e7a
Tapio Vierros <tapiovierros@gmail.com>
2013-02-17 19:49:42 +0200

    Switched back to Bootstrap and added slider value display.

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,26 @@
+(function() {
 
-$("#menu").tabs();
-$("button").button();
+	// Create
+
+	function checkGameVars() {
+		//if (
+	}
+
+	$("#enemies").change(function() {
+		$(this).next("output").html($(this).val());
+	});
+	$("#allies").change(function() {
+		$(this).next("output").html($(this).val());
+	});
+
+	$("#to-create").click(function() { $('#menu a[href="#create"]').tab('show'); });
+	$("#to-join").click(function() { $('#menu a[href="#join"]').tab('show'); });
+
+	// Settings
+
+	var particleLegend = [ "None", "Few", "Some", "Lots", "Insane" ];
+	$("#particles").change(function() {
+		$(this).next("output").html(particleLegend[$(this).val()]);
+	});
+
+})();
